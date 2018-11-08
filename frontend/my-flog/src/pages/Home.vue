de631c8eb76283e0f063fe33695a830a40cb206d
jerry <1126913855@qq.com>
2018-11-08 17:06:56 +0800

    前端 部分 vue cli

---
+++ frontend/my-flog/src/pages/Home.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>{{ title }}</div>
+</template>
+<script>
+export default {
+    name: 'Home',
+    data() {
+        return {
+            title: '这是我的博客 主页'
+        }
+    }
+}
+</script>
