ad1fd197910f250705dd2813f6b8cb43b759e516
jerry <1126913855@qq.com>
2018-11-21 20:00:44 +0800

    commit 引入 element 作为组件

--- frontend/my-flog/src/App.vue
+++ frontend/my-flog/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" :src=imgPath />
+    <!-- <img alt="Vue logo" :src=imgPath /> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <home></home>
   </div>
@@ -17,18 +17,34 @@ export default {
   data() {
     return {
       imgPath: require('./assets/logo.png')
-    } 
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+<style lang="scss">
+// @import 'assets/styles/base.scss'
+html,
+body {
+  width: 100%;
+  height: 100%;
+  min-width: 1024px;
+  overflow: hidden;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+* {
+  font-family: Microsoft YaHei;
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+
+#app {
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
 }
 </style>
--- frontend/my-flog/src/pages/Home.vue
+++ frontend/my-flog/src/pages/Home.vue
@@ -1,17 +1,59 @@
 <template>
-    <div>
-        <div>{{ title }}</div>
-        <img alt="Vue logo" :src=imgPath />
-    </div>
+  <div class="main">
+    <!-- <div>{{ title }}</div>
+        <img alt="Vue logo" :src=imgPath /> -->
+    <el-container>
+      <el-header>
+        <el-row>
+          <el-col :span="8">
+            Header
+          </el-col>
+          <el-col :span="8">
+            Headerl
+          </el-col>
+           <el-col :span="8">
+            Header1
+          </el-col>
+        </el-row>
+      </el-header>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-main>Main</el-main>
+         <el-aside width="200px">Aside</el-aside>
+      </el-container>
+    </el-container>
+  </div>
+
 </template>
 <script>
 export default {
-  name: "Home",
+  name: 'Home',
   data() {
     return {
-      title: "这是我的博客 主页",
-      imgPath: require("../assets/logo.png")
-    };
+      title: '这是我的博客 主页',
+      imgPath: require('../assets/logo.png')
+    }
   }
-};
+}
 </script>
+<style lang="scss">
+.main {
+  height: 100%;
+  .el-header {
+    background: gray;
+    height: 100px;
+  }
+  .el-container {
+    height: 100%;
+    background: wheat;
+  }
+  .el-main {
+    background: white;
+    height: 70%;
+  }
+  .el-aside {
+    height: 70%;
+  }
+}
+</style>
+
