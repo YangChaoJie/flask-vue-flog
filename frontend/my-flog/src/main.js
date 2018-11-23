727c786e41e7a922fefff152eae7a5855b17355e
jerry <1126913855@qq.com>
2018-11-23 15:27:35 +0800

    commit 引入 Vue router

--- frontend/my-flog/src/main.js
+++ frontend/my-flog/src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import ElementUI from 'element-ui'
+import VueRouter from 'vue-router'
 import 'element-ui/lib/theme-chalk/index.css'
 Vue.config.productionTip = false
 Vue.use(ElementUI)
+Vue.use(VueRouter)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
